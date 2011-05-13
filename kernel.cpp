629bd03ee0cad69b261a1c22b18387e9a016bdf0
Eirik Brandtzæg <eirikb@eirikb.no>
2011-05-13 18:12:18 +0200

    Reverted random commited test
    Change-Id: I06706faa31e1c19793a8d354500ac1ccf2751e24

--- kernel.cpp
+++ kernel.cpp
@@ -1,37 +1,8 @@
 #include "console.h"
 
-void malloc(size_t size) 
-{
-}
-
-void free(void *p)
-{
-}
-
-
-void *operator new(size_t size)
-{
-        malloc(size);
-}
- 
-void *operator new[](size_t size)
-{
-        malloc(size);
-}
- 
-void operator delete(void *p)
-{
-        free(p);
-}
- 
-void operator delete[](void *p)
-{
-        free(p);
-}
-
 int main(void)
 {
-    Console console = new Console();
+    Console console;
     console.write("Hello, world!");
     console.nl();
     console.write("Welcome to w8...");
